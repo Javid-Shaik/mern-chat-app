9a60ced0ee2254bf02ababa582272da364c66c1d
Javid-Shaik <101865673+Javid-Shaik@users.noreply.github.com>
2024-08-11 01:40:39 +0530

    Added date filed

--- frontend/src/components/ScrollableChat.js
+++ frontend/src/components/ScrollableChat.js
@@ -8,44 +8,94 @@ import {
   isSameUser,
 } from "../config/ChatLogics";
 import { ChatState } from "../Context/ChatProvider";
+import moment from 'moment';
+
+
+const groupMessagesByDate = (messages) => {
+  const groupedMessages = [];
+  let currentDate = null;
+
+  messages.forEach((message) => {
+    const messageDate = moment(message.createdAt).format('YYYY-MM-DD');
+
+    if (messageDate !== currentDate) {
+      groupedMessages.push({ date: messageDate, messages: [message] });
+      currentDate = messageDate;
+    } else {
+      groupedMessages[groupedMessages.length - 1].messages.push(message);
+    }
+  });
+
+  return groupedMessages;
+};
+
 
 const ScrollableChat = ({ messages }) => {
   const { user } = ChatState();
+  
+  // Group messages by date
+  const groupedMessages = groupMessagesByDate(messages);
 
   return (
     <ScrollableFeed>
-      {messages &&
-        messages.map((m, i) => (
-          <div style={{ display: "flex" }} key={m._id}>
-            {(isSameSender(messages, m, i, user._id) ||
-              isLastMessage(messages, i, user._id)) && (
-              <Tooltip label={m.sender.name} placement="bottom-start" hasArrow>
-                <Avatar
-                  mt="7px"
-                  mr={1}
-                  size="sm"
-                  cursor="pointer"
-                  name={m.sender.name}
-                  src={m.sender.pic}
-                />
-              </Tooltip>
-            )}
+      {groupedMessages.map((group, index) => (
+        <div key={index}>
+          {/* Display the date header */}
+          <div
+            style={{
+              textAlign: "center",
+              margin: "20px 0",
+              color: "#fff",
+            }}
+          >
             <span
               style={{
-                backgroundColor: `${
-                  m.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"
-                }`,
-                marginLeft: isSameSenderMargin(messages, m, i, user._id),
-                marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10,
-                borderRadius: "20px",
+                backgroundColor: "#2D3748", // Dark background color
+                borderRadius: "10px",
                 padding: "5px 15px",
-                maxWidth: "75%",
+                display: "inline-block",
+                color: "#fff", // Ensure text color contrasts with background
+                fontSize: "0.875rem" // Optional: adjust font size if needed
               }}
             >
-              {m.content}
+              {moment(group.date).format('MMMM D, YYYY')}
             </span>
           </div>
-        ))}
+          {group.messages.map((m, i) => (
+            <div style={{ display: "flex" }} key={m._id}>
+              {(isSameSender(group.messages, m, i, user._id) ||
+                isLastMessage(group.messages, i, user._id)) && (
+                <Tooltip label={m.sender.name} placement="bottom-start" hasArrow>
+                  <Avatar
+                    mt="7px"
+                    mr={1}
+                    size="sm"
+                    cursor="pointer"
+                    name={m.sender.name}
+                    src={m.sender.pic}
+                  />
+                </Tooltip>
+              )}
+              <span
+                style={{
+                  backgroundColor: `${
+                    m.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"
+                  }`,
+                  marginLeft: isSameSenderMargin(group.messages, m, i, user._id),
+                  marginTop: isSameUser(group.messages, m, i, user._id) ? 3 : 10,
+                  borderRadius: "20px",
+                  padding: "5px 15px",
+                  maxWidth: "75%",
+                }}
+              >
+                {m.content}
+                <br />
+                <small>{moment(m.createdAt).format('hh:mm A')}</small>
+              </span>
+            </div>
+          ))}
+        </div>
+      ))}
     </ScrollableFeed>
   );
 };
